1756b7e1e6b9c20ccec4a363aae656b42cdf0dd7
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-07 12:54:35 +0200

    exo_07_a.rb

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+# .gets.chomp demande à l'utilisateur d'entrer une information, en l'occurrence, le nom avec la phrase au dessus

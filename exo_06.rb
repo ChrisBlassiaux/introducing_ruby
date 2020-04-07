e797d37fa03a2da018cc0f176ca04d66d32b389e
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-07 12:48:43 +0200

    exo_06.rb

---
+++ exo_06.rb
@@ -0,0 +1,13 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# Nous appelons les variables pour faire une multiplaction pour avoir le nombre d'heures de travail
+
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+
+# Il y a une erreur puisqu'il y a pas de variable "number_of_minutes_in_an_hour"

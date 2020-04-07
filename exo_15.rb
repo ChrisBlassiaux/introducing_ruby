ea1298e5bab0212840eb3542cac79154587865cb
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-07 15:26:49 +0200

    exo_15.rb

--- exo_15.rb
+++ exo_15.rb
@@ -1,10 +1,11 @@
 puts "Veuillez entrer votre année de naissance"
 print "> "
 year = gets.chomp.to_i
-
+year_save = year
 
 while year < 2020 do 
     year = year + 1
     puts year
+    puts "Vous aviez #{year - year_save} ans"
 end
 

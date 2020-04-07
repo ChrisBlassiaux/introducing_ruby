3aeb2898af204fc303475e29919721ce657459de
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-07 13:01:06 +0200

    exo_07_c.rb

---
+++ exo_07_c.rb
@@ -0,0 +1,5 @@
+user_name = gets.chomp
+puts user_name
+
+# Les 3 programmes effectuent la même chose, sauf que pour le 2eme programme, on indique à l'utilisateur qu'il faut taper avec le >
+# le 3eme (c) est le moins bien concernant l'expérience utilisateur

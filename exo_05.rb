c4364347ca76463e63443be9c250780ffc79f84c
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-07 12:42:08 +0200

    exo_05.rb

---
+++ exo_05.rb
@@ -0,0 +1,32 @@
+# Elle affiche à l'écran la phrase entre ""
+puts "On va compter le nombre d'heures de travail à THP"
+# Elle affiche à l'écran "Travail :" en concaténant le résultat d'une multiplication 
+puts "Travail : #{10 * 5 * 11}"
+# Elle affiche à l'écran "En minutes ça fait :" en concaténant le résultat d'une multiplication 
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# Elle affiche à l'écran la phrase entre ""
+puts "Et en secondes ?"
+# Elle affiche à l'écran le resultat de la multiplication
+puts 10 * 5 * 11 * 60 * 60
+# Elle affiche à l'écran la phrase entre ""
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# Elle affiche à l'écran le resultat du calcul
+puts 3 + 2 < 5 - 7
+# Elle affiche à l'écran la phrase intérrogative puis la réponse avec la concaténation
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# Elle affiche à l'écran la phrase intérrogative puis la réponse avec la concaténation
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# Elle affiche à l'écran la phrase entre ""
+puts "Ok, c'est faux alors !"
+# Elle affiche à l'écran la phrase entre ""
+puts "C'est drôle ça, faisons-en plus :"
+# Elle affiche à l'écran la phrase intérrogative puis la réponse avec la concaténation
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+# Elle affiche à l'écran la phrase intérrogative puis la réponse avec la concaténation
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+# Elle affiche à l'écran la phrase intérrogative puis la réponse avec la concaténation
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+
+
+# #{} ceci est une concaténation de string avec des numbers

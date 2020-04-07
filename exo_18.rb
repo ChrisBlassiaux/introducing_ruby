bc8e85e9dbcc313999abf23d07bdcc18835e882b
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-07 17:34:45 +0200

    exo_19.rb

--- exo_18.rb
+++ exo_18.rb
@@ -3,7 +3,7 @@ array_email = []
 compteur = 0
 while compteur < 50
     array_email << "jean.dupont." + compteur.to_s + "@email.fr"
-    compteur += 01
+    compteur += 1
 end
 
 print array_email
---
+++ exo_19.rb
@@ -0,0 +1,11 @@
+
+
+
+
+compteur = 0
+while compteur < 50
+    if compteur % 2 == 0
+    puts "jean.dupont." + compteur.to_s + "@email.fr"
+    end
+    compteur += 1
+end
